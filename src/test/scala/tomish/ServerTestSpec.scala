33aa1c480ced027d87056f0f15a798cf1a9e8ee1
Tomohiro Ishiwata <tomohiro.ishiwata@gmail.com>
2019-09-16 01:04:08 +0200

    Message to GameEvent graph

--- src/test/scala/tomish/ServerTestSpec.scala
+++ src/test/scala/tomish/ServerTestSpec.scala
@@ -1,26 +1,128 @@
 package tomish
 
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.model.ws.{BinaryMessage, Message, TextMessage}
-import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.http.scaladsl.server.Directives
+import akka.http.scaladsl.testkit.{ScalatestRouteTest, WSProbe}
+import akka.stream.{ActorMaterializer, FlowShape, OverflowStrategy}
+import akka.stream.scaladsl.{Flow, GraphDSL, Merge, Sink, Source}
 import org.scalatest._
 
-class ServerTestSpec extends FlatSpec with Matchers {
-  "object" should "create empty GameService" in {
+class ServerTestSpec extends FunSuite with Matchers with ScalatestRouteTest  {
+  test("should create empty GameService") {
     new GameService()
   }
 
-  "object" should "connect to the GameService websocket" in {
+  test("should connect to the GameService websocket") {
+    assertWebSocket("John") { wsClient =>
+      // check response for WS upgrade headers
+      isWebSocketUpgrade shouldEqual true
+    }
+  }
 
+  test("should respond with correct message") {
+    assertWebSocket("John") { wsClient =>
+      wsClient.expectMessage("welcome John")
+      wsClient.sendMessage("hello")
+      wsClient.expectMessage("hello")
+    }
+  }
+
+  test("should register player") {
+    assertWebSocket("John") { wsClient =>
+      wsClient.expectMessage("welcome John")
+    }
+  }
+
+  test("should register multiple players") {
+    val gameService = new GameService()
+    val wsClient1 = WSProbe()
+    val wsClient2 = WSProbe()
+
+    WS(s"/?playerName=john", wsClient1.flow) ~> gameService.webSocketRoute -> check {
+      wsClient1.expectMessage("[{\"name\": \"john\"}]")
+    }
+
+    WS(s"/?playerName=john", wsClient1.flow) ~> gameService.webSocketRoute -> check {
+      wsClient2.expectMessage("[{\"name\": \"john\"},{\"name\": \"alice\"}]")
+    }
+  }
+
+  def assertWebSocket(playerName: String)(assertions: WSProbe => Unit) : Unit = {
+    val gameService = new GameService()
+    val wsClient = WSProbe()
+
+    WS(s"/?playerName=$playerName", wsClient.flow) ~> gameService.webSocketRoute -> check {
+      assertions(wsClient)
+    }
   }
 }
 
-class GameService() {
-  def greeter: Flow[Message, Message, Any] = Flow[Message].mapConcat {
-    case textMessage : TextMessage =>
-      TextMessage(Source.single("Hello") ++ textMessage.textStream ++ Source.single("!")) :: Nil
-    case binaryMessage : BinaryMessage =>
-      // ignore binary messages but drain content to avoid the stream being clogged
-      binaryMessage.dataStream.runWith(Sink.ignore)
-      Nil
+class GameService() extends Directives {
+  implicit val actorSystem = ActorSystem()
+  implicit val actorMaterializer = ActorMaterializer()
+
+  val webSocketRoute = (get & parameter("playerName")) { playerName =>
+    handleWebSocketMessages(flow(playerName))
+  }
+
+  val gameAreaActor = actorSystem.actorOf(Props[GameAreaActor], "gameAreaActor")
+  val playerActorSource = Source.actorRef[GameEvent](5, OverflowStrategy.fail)
+
+  def flow(playerName: String): Flow[Message, Message, Any] = Flow.fromGraph(GraphDSL.create(playerActorSource) {
+    implicit builder => playerActor =>
+    import GraphDSL.Implicits._
+
+    val materialization = builder.materializedValue.map(playerActorRef => PlayerJoined(Player(playerName), playerActorRef))
+    val messagePassingFlow = builder.add(Flow[Message].map(m => m))
+    val merge = builder.add(Merge[GameEvent](2))
+
+    val messagesToGameEventsFlow = builder.add(Flow[Message].map {
+      case TextMessage.Strict(txt) => PlayerMoveRequest(playerName, txt)
+    })
+
+    val gameEventsToMessagesFlow = builder.add(Flow[GameEvent].map {
+      case PlayerChanged(players) => {
+        import spray.json._
+        import DefaultJsonProtocol._
+
+        val playerFormat = jsonFormat1(Player)
+        TextMessage(players.toJson.toString)
+      }
+    })
+
+    val gameAreaActorSink = Sink.actorRef[GameEvent](gameAreaActor, PlayerLeft(playerName))
+
+    materialization ~> merge
+    messagesToGameEventsFlow ~> merge
+    merge ~> gameAreaActorSink
+
+    playerActor ~> gameEventsToMessagesFlow
+
+    FlowShape(messagesToGameEventsFlow.in, gameEventsToMessagesFlow.out)
+  })
+}
+
+class GameAreaActor extends Actor {
+  val players = collection.mutable.LinkedHashMap[String, PlayerWithActor]()
+
+  override def receive: Receive = {
+    case PlayerJoined(player, actor) => players += (player.name -> PlayerWithActor(player, actor))
+    case PlayerLeft(playerName) => players -= (playerName)
+    case PlayerMoveRequest(playerName, d) => ???
+  }
+
+  def notifyPlayersChanged(): Unit = {
+    players.values.foreach(_.actor ! PlayerChanged(players.values.map(_.player)))
   }
 }
+
+trait GameEvent
+case class PlayerJoined(player: Player, actor: ActorRef) extends GameEvent
+case class PlayerLeft(playerName: String) extends GameEvent
+case class PlayerMoveRequest(playerName: String, direction: String) extends GameEvent
+case class PlayerChanged(players: Iterable[Player]) extends GameEvent
+
+case class Player(name: String)
+case class PlayerWithActor(player: Player, actor: ActorRef)
+
